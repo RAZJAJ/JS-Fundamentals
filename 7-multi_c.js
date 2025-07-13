45b91480251bf7234ac5e877bfafb718c4966353
RAZJAJ <109910534+RAZJAJ@users.noreply.github.com>
2025-07-13 18:52:25 +0100

    Delete 7-multi_c.js

--- 7-multi_c.js
+++
@@ -1,9 +0,0 @@
-const count = Number(process.argv[2]);
-
-if (Number.isInteger(count)) {
-  for (let i = 0; i < count; i++) {
-    console.log('C is fun');
-  }
-} else {
-  console.log('Missing number of occurrences');
-}
